a96de7a4d6b1a2a04757c20a807fd91cc0122209
olawale <whalesarowolo@gmail.com>
2025-02-26 01:33:46 +0000

    Comment moderation API

---
+++ moderation_api.py
@@ -0,0 +1,43 @@
+from pydoc import pipe_pager
+from sre_parse import Tokenizer
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import BertForSequenceClassification, BertTokenizer, TextClassificationPipeline
+
+# Initialize FastAPI app
+app = FastAPI()
+
+model_path = "JungleLee/bert-toxic-comment-classification"
+tokenizer = BertTokenizer.from_pretrained(model_path)
+model = BertForSequenceClassification.from_pretrained(model_path, num_labels=2)
+pipeline = TextClassificationPipeline(model=model, tokenizer=tokenizer)
+
+#define custom keywords and function to classify comments
+
+custom_keywords = ["money", "sale", "i sell", "call me on", "price", "cheap", "book me", "call me", "buy", "affordable price", "order"]
+
+def is_inappropriate(comment):
+    #use pre-trained model
+    result = pipeline(comment)[0]
+    if result['label'] == 'toxic' and result['score']>0.5:
+        return {"status": "Rejected", "Reason": "Toxic comment detected"}
+
+    #Check for custom keywords
+    for keyword in custom_keywords:
+        if result['label'] == 'non-toxic' and result['score']>0.5 and keyword in comment.lower():
+            return{"status": "Rejected", "Reason": "Comment contains forbidden words"}
+    return{"status":"Approved", "Reason": "Safe comment"}
+     
+#comment moderation endpoint
+@app.post("/moderate")
+async def moderate(comment: str):
+    return is_inappropriate(comment)
+
+#Root endpoint
+@app.get("/")
+async def root():
+    return {"message": "Forum Comment Moderator API is running"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
